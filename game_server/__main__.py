a2f04c870480b184163b891cd4e0e5ee7c2d9bfb
Meg Anderson <megretson@gmail.com>
2022-03-26 19:51:55 -0500

    initial addition of the database

--- game_server/__main__.py
+++ game_server/__main__.py
@@ -3,15 +3,20 @@
 import connexion
 
 from game_server import encoder
+from game_server.db.database import setup_db
+from flask import g
+from flask_pymongo import PyMongo
 
 
 def main():
+    global app
+    global mongo
     app = connexion.App(__name__, specification_dir='./openapi/')
+    setup_db(app.app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
                 arguments={'title': 'Clue Game Server'},
                 pythonic_params=True)
-
     app.run(port=8080)
 
 
---
+++ game_server/db/database.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+MONGO = None
+
+def setup_db(flask_app):
+    global MONGO
+    flask_app.config["MONGO_URI"] = "mongodb://localhost:27017/clueDB"
+    MONGO = PyMongo(flask_app)
+
+def get_games_collection():
+    assert MONGO is not None
+    return MONGO.db.get_collection("Games")
+
+def get_players_collection():
+    assert MONGO is not None
+    return MONGO.db.get_collection("Players")
+
+def get_games():
+    return get_games_collection().find()
+
+def get_players():
+    return get_players_collection().find()
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
